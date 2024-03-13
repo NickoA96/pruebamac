3d31985878c831c96a41a737b559eb25c3e4fed8
macnico <nicolas.albornoz996@gmail.com>
2024-03-13 19:10:44 -0300

    probando ramas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let resu = prompt("Cual es tu nombre ?")
 let contenido = document.getElementById("titulo").textContent  
 
-
+console.log("aaa")
 document.getElementById("titulo").innerText= `${contenido} ${resu}`
