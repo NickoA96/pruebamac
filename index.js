2690e47dd7cb38d6579982389242d6693252a899
macnico <nicolas.albornoz996@gmail.com>
2024-03-14 04:47:43 -0300

    ultimo commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let resu = prompt("Cual es tu nombre ?")
 let contenido = document.getElementById("titulo").textContent  
-
 console.log("Morito te amo")
+console.log('Hema te amo');
+
 document.getElementById("titulo").innerText= `${contenido} ${resu}`
