4e076c4c3ad9d8640ee0b4ec14dc87fbd566ac07
macnico <nicolas.albornoz996@gmail.com>
2024-03-14 05:06:56 -0300

    nueva rama con el nombre cambiado

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ let resu = prompt("Cual es tu nombre ?")
 let contenido = document.getElementById("titulo").textContent  
 console.log("Morito te amo")
 console.log('Hema te amo');
+console.log("Abril")
 
 document.getElementById("titulo").innerText= `${contenido} ${resu}`
